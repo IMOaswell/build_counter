382f7b558d3ae99084640a46b80704e663b129a3
IMOitself <russelljonnbautista@gmail.com>
2024-07-21 11:22:56 +0800

    feat: add new activity for picking package names to be open on MainActivity

---
+++ java/imo/build_counter/PackagePickerActivity.java
@@ -0,0 +1,15 @@
+package imo.build_counter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PackagePickerActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_picker);
+        
+    }
+    
+}
